d6b9638b0f5dd07d074f39624c5d2b7fc3903be3
saipranay47 <saipranayadepu07@gmail.com>
2023-05-15 01:50:07 +0530

    fetch url update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import styles from './page.module.css'
 
 const fetchNotionDb = async () => {
-  const res = await fetch('http://localhost:3000/api/getData')
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getData`)
   const data = await res.json()
   return (data);
 }
